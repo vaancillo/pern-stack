8c9425ab1cf065749828466588f321ef21f70c61
vancillo <yaredbane@gmail.com>
2023-10-28 22:01:23 -0600

    backend setup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(morgan('dev'))
+
+app.listen(4000)
+console.log("Server is running on port 4000")

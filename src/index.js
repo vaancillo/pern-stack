30185311e18442dc9b5cfd10962a88fe55e430a8
vancillo <yaredbane@gmail.com>
2023-10-31 11:06:00 -0600

    defining routes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
 
+const taskRoutes = require('./routes/tasks.routes')
+
 const app = express()
 
 app.use(morgan('dev'))
+
+app.use(taskRoutes)
 
 app.listen(4000)
 console.log("Server is running on port 4000")
---
+++ src/routes/tasks.routes.js
@@ -0,0 +1,21 @@
+const { Router } = require('express')
+
+const router = Router();
+
+router.get('/tasks', (req, res) =>{
+    res.send('retrieving a list of task')
+})
+router.get('/tasks/10', (req, res) =>{
+    res.send('retrieving a single task')
+})
+router.post('/tasks', (req, res) =>{
+    res.send('creating a task')
+})
+router.delete('/tasks', (req, res) =>{
+    res.send('deleting a task')
+})
+router.put('/tasks', (req, res) =>{
+    res.send('updating a task')
+})
+
+module.exports = router

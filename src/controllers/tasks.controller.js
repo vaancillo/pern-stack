962c3ab3cf87acedf5ffeaba427eea471e4ef122
vancillo <yaredbane@gmail.com>
2023-11-04 22:23:21 -0600

    Add createTask functionality and middleware for JSON parsing

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,3 +1,5 @@
+const pool = require('../db')
+
 const getAllTasks = async (req, res) =>{
     res.send('retrieving a list of task')
 }
@@ -7,14 +9,22 @@ const getTask = (req, res) =>{
 }
 
 const createTask = async (req, res) =>{
-    res.send('creating a task');
+    const { title, description } = req.body
+
+    try {
+        const result = await pool.query('INSERT INTO task (title, description) VALUES ($1, $2) RETURNING *', [title, description])
+        res.json(result.rows[0])
+    } catch (error) {
+        console.log(error.message)
+        res.json({error: error.message})
+    }
 }
 
-const deleteTask = async (req, res) =>{
+const deleteTask = (req, res) =>{
     res.send('deleting a task');
 }
 
-const updateTask = async (req, res) =>{
+const updateTask = (req, res) =>{
     res.send('updating a task');
 }
 

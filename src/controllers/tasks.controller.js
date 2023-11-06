efaa5d60ebd3e6da201692ea2a154080b4e3b31a
vancillo <yaredbane@gmail.com>
2023-11-05 21:03:07 -0600

    Add route and controller function to update a task

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -48,8 +48,14 @@ const deleteTask = async (req, res) => {
   res.sendStatus(204)
 }
 
-const updateTask = (req, res) => {
-  res.send('updating a task')
+const updateTask = async (req, res) => {
+  const { id } = req.params
+  const { title, description } = req.body
+  const result = await pool.query('UPDATE task SET title = $1, description = $2 WHERE id = $3 RETURNING *', [title, description, id])
+  if (result.rows.length === 0) {
+    return res.status(404).json({ message: 'Task not found' })
+  }
+  return res.json(result.rows[0])
 }
 
 module.exports = {

a58fea2c0d12c08ca2c050f5f13b94957dc8e4ef
vancillo <yaredbane@gmail.com>
2023-11-06 22:14:50 -0600

    form and state to create tasks

--- client/src/components/TaskForm.jsx
+++ client/src/components/TaskForm.jsx
@@ -1,6 +1,68 @@
+import { Button, Card, CardContent, Grid, TextField, Typography } from '@mui/material'
+import { useState, useEffect } from 'react'
+
 const TaskForm = () => {
+  const [task, setTask] = useState({
+    title: '',
+    description: ''
+  })
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(task)
+  }
+
+  const handleChange = (e) => {
+    setTask({ ...task, [e.target.name]: e.target.value })
+  }
+
   return (
-    <div>TaskForm</div>
+    <Grid container direction='column' alignItems='center' justifyContent='center'>
+      <Grid item xs={3}>
+        <Card
+          sx={{ mt: 5 }} style={{
+            backgroundColor: '#1e272e',
+            padding: '1rem'
+          }}
+        >
+          <Typography variant='5' textAlign='center' color='white'>
+            Create Task
+          </Typography>
+          <CardContent>
+            <form onSubmit={handleSubmit}>
+              <TextField
+                variant='filled'
+                label='Write your title'
+                sx={{
+                  display: 'block',
+                  margin: '.5rem 0'
+                }}
+                name='title'
+                onChange={handleChange}
+                inputProps={{ style: { color: 'white' } }}
+                InputLabelProps={{ style: { color: 'white' } }}
+              />
+              <TextField
+                variant='filled'
+                label='Write your description'
+                multiline
+                rows={4}
+                sx={{
+                  display: 'block',
+                  margin: '.5rem 0'
+                }}
+                name='description'
+                onChange={handleChange}
+                inputProps={{ style: { color: 'white' } }}
+                InputLabelProps={{ style: { color: 'white' } }}
+              />
+              <Button variant='contained' type='submit'>
+                Create Task
+              </Button>
+            </form>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
   )
 }
 export default TaskForm

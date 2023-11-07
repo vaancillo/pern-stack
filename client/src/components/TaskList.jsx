e79f4ae5cab8b19422dfc90b1ac24c3427b09383
vancillo <yaredbane@gmail.com>
2023-11-07 09:58:37 -0600

    rendering task in the tasklist component

--- client/src/components/TaskList.jsx
+++ client/src/components/TaskList.jsx
@@ -1,6 +1,53 @@
+import { useEffect, useState } from 'react'
+import { Card, CardContent, Typography, Button } from '@mui/material'
+
 const TaskList = () => {
+  const [task, setTask] = useState([])
+
+  const loadTask = async () => {
+    const response = await fetch('http://localhost:4000/tasks')
+    const data = await response.json()
+    setTask(data)
+  }
+  useEffect(() => {
+    loadTask()
+  }, [])
+
   return (
-    <div>TaskList</div>
+    <>
+      <h1>Task List</h1>
+      {
+        task.map((task) => (
+          <Card
+            key={task.id} style={{
+              marginBottom: '.7rem',
+              backgroundColor: '#1e272e'
+            }}
+          >
+            <CardContent style={{
+              display: 'flex',
+              justifyContent: 'space-between'
+            }}
+            >
+              <div style={{ color: 'white' }}>
+                <Typography>{task.title}</Typography>
+                <Typography>{task.description}</Typography>
+              </div>
+
+              <div>
+                <Button variant='contained' color='inherit' onClick={() => console.log('edit')}>
+                  Edit
+                </Button>
+                <Button variant='contained' color='warning' onClick={() => console.log('Delete')} style={{ marginLeft: '.5rem' }}>
+                  Delete
+                </Button>
+              </div>
+            </CardContent>
+          </Card>
+        ))
+      }
+
+    </>
   )
 }
 export default TaskList
